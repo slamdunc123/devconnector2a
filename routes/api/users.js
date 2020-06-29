9b2130f2058a569b871535997ad0049eaa1c62fe
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-06-29 22:04:20 +0100

    back end posts routes set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,20 @@ const User = require('../../models/User');
 // @router  GET api/users - http://localhost:5000/api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Users test route'));
+// router.get('/', (req, res) => res.send('Users test route'));
+
+// @router  GET api/users - http://localhost:5000/api/users
+// @desc    Get all users
+// @access  Public
+router.get('/', async (req, res) => {
+	try {
+		const users = await User.find();
+		res.json(users);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
 
 // @router  POST api/users - http://localhost:5000/api/users
 // @desc    Register user

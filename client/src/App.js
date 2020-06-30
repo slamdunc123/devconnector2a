3b71cd12a5db492ec81fb503f85c819544293657
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-06-30 22:21:26 +0100

    front-end react and redux set up, alerts added

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => (
+	<Provider store={store}>
+		<Router>
+			<Navbar />
+			<Route exact path='/' component={Landing} />
+			<section className='container'>
+				<Alert />
+				<Switch>
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/login' component={Login} />
+				</Switch>
+			</section>
+		</Router>
+	</Provider>
+);
+
+export default App;
